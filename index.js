27e64fd997dc9a36129f243abaf259da5aa5235f
Gabriel <vytorgabriel123v@gmail.com>
2020-12-02 17:49:42 -0300

    RestAPI

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const consign = require('consign');
+const bodyParser = require('body-parser');
+const validatorExpress = require('express-validator');
+
+let app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(validatorExpress());
+
+consign().include('routes').include('utils').into(app);
+
+app.listen(3000, '127.0.0.1', ()=>{
+
+    console.log('servidor rodando!');
+
+});
